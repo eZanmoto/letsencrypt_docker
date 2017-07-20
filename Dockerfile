a2e06930e19de575d4ebf65659f44ac30528dd96
Sean Kelleher <ezanmoto@gmail.com>
2017-07-20 13:58:32 +0000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Copyright 2017 Sean Kelleher. All rights reserved.
+
+# This Dockerfile is for an image to create Let's Encrypt credentials. See
+# `gen_keys.sh` for details.
+
+FROM debian:8.6
+
+RUN \
+    apt-get update \
+    && apt-get install --quiet --assume-yes \
+        wget
+
+RUN \
+    wget https://dl.eff.org/certbot-auto \
+    && chmod a+x certbot-auto \
+    && ./certbot-auto --non-interactive \
+    || true
+
+COPY gen.sh /
+
+ENTRYPOINT [ "bash", "/gen.sh" ]
---
+++ gen.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Copyright 2017 Sean Kelleher. All rights reserved.
+
+# `$0 <http|https> <out-dir> <domains>` generates Let's Encrypt credentials for
+# a list of comma-separated `domains` to `out-dir` using the local `http` or
+# `https` port of the current host, which must be reachable using `domains`.
+
+set -o errexit
+
+if [ $# -ne 2 ] ; then
+    echo "usage: $0 <http|https> <domains>" >&2
+    exit 1
+fi
+
+method=$1
+domains=$2
+
+port=80
+if [ "$method" == "https" ] ; then
+    method='tls-sni'
+    port=443
+elif [ "$method" != "http" ] ; then
+    echo "usage: $0 <http|https> <out-dir> <domains>" >&2
+    exit 1
+fi
+
+./certbot-auto \
+    certonly \
+    --standalone \
+    --preferred-challenges "$method" \
+    --verbose \
+    --register-unsafely-without-email \
+    --non-interactive \
+    --agree-tos \
+    --domains "$domains" \
